ada1aa29459f8f128afde4c7981023298a2742f9
robcio89 <robcio89@gmail.com>
2015-12-01 22:22:41 +0100

    corrected encrypt method

--- UnitTest/CaesarCipherTest.java
+++ UnitTest/CaesarCipherTest.java
@@ -15,4 +15,26 @@ public class CaesarCipherTest extends TestCase {
 
 		assertEquals(answer, c.encrypt(input, key));
 	}
+
+	public void testEncrypt2() throws Exception
+	{
+		CaesarCipher c = new CaesarCipher();
+
+		String input = "First Legion";
+		int key = 23;
+		String answer = "Cfopq Ibdflk";
+
+		assertEquals(answer, c.encrypt(input, key));
+	}
+
+	public void testEncrypt3() throws Exception
+	{
+		CaesarCipher c = new CaesarCipher();
+
+		String input = "First Legion";
+		int key = 17;
+		String answer = "Wzijk Cvxzfe";
+
+		assertEquals(answer, c.encrypt(input, key));
+	}
 }
--- src/CaesarCipher.java
+++ src/CaesarCipher.java
@@ -5,24 +5,47 @@ public class CaesarCipher
 {
 	public String encrypt(String input, int key)
 	{
-		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String alphabetUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String alphabetLow = "abcdefghijklmnopqrstuvwxyz";
 
 		StringBuilder encrypted = new StringBuilder(input);
-		String shiftedAlphabet = alphabet.substring(key) + alphabet.substring(0, key);
+
+		String shiftedAlphabetUp = alphabetUp.substring(key) + alphabetUp.substring(0, key);
+		String shiftedAlphabetLow = alphabetLow.substring(key) + alphabetLow.substring(0, key);
 
 		for (int i =0; i < encrypted.length(); i++)
 		{
 			char currChar = encrypted.charAt(i);
 
-			int idx = alphabet.indexOf(currChar);
-
-			if (idx != -1)
+			if (alphabetUp.indexOf(currChar) != -1)
 			{
-				char newChar = shiftedAlphabet.charAt(idx);
+				int idx = alphabetUp.indexOf(currChar);
 
-				encrypted.setCharAt(i, newChar);
+				if (idx != -1)
+				{
+					char newChar = shiftedAlphabetUp.charAt(idx);
+
+					encrypted.setCharAt(i, newChar);
+				}
+			}
+			else if (alphabetLow.indexOf(currChar) != -1)
+			{
+				int idx = alphabetLow.indexOf(currChar);
+
+				if (idx != -1)
+				{
+					char newChar = shiftedAlphabetLow.charAt(idx);
+
+					encrypted.setCharAt(i, newChar);
+				}
 			}
 		}
 		return encrypted.toString();
+	}
+
+	public void testCaesar()
+	{
+		// TODO
+		// Read file
 	}
 }
